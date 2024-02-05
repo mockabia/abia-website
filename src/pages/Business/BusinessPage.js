5f518a132525de79d3d1348b10b372469ae15f34
swathiabia <117815710+swathiabia@users.noreply.github.com>
2024-02-05 16:59:49 +0530

    page new

--- src/pages/Business/BusinessPage.js
+++ src/pages/Business/BusinessPage.js
@@ -1,30 +1,16 @@
-import React, { useEffect, useState } from "react";
-import { Navigate, useLocation, useNavigate } from "react-router-dom";
-import LayoutVendor from "../../layouts/Layout/LayoutVendor";
-import * as BusinessJS from "./Business";
-
+import React from "react";
 
 const BusinessPage = (props) => {
-  let navigate = useNavigate();
-  const [dataSet, setDataSet] = useState(false);
-  const [inputs, setInputs] = useState({});
-  const [inputsErrors, setInputsErrors] = useState({});
-
-  useEffect(() => {
-    //BusinessJS.fetchbusiness(setInputs, setDataSet);
-  }, []);
-
   return (
     <>
-      <LayoutVendor>
         <div>
           <div className="main-content">
           <h1 className="main-header">Business Page Demo</h1>
           {/* <div className="content" dangerouslySetInnerHTML={{__html: pageContent.detail}}></div> */}
           </div>
         </div>
-      </LayoutVendor>
     </>
   );
 };
+
 export default BusinessPage;
